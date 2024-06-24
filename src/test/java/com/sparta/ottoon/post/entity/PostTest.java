1f15940bab8466fc2d512da9cc9f532a57820708
serim01 <kkhksl13@gmail.com>
2024-06-24 14:37:17 +0900

    [#28] add: post test 코드 추가 & fix: ProfileService transactional 추가

---
+++ src/test/java/com/sparta/ottoon/post/entity/PostTest.java
@@ -0,0 +1,52 @@
+package com.sparta.ottoon.post.entity;
+
+import com.sparta.ottoon.fixtureMonkey.FixtureMonkeyUtil;
+import com.sparta.ottoon.post.dto.PostRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@DisplayName("게시글 entity 테스트")
+public class PostTest {
+    Post post;
+
+    @BeforeEach
+    void setUp() {
+        post = FixtureMonkeyUtil.Entity.toPost();
+    }
+
+    @DisplayName("게시글 등록")
+    @Test
+    void test1() {
+        // given
+        String content = "content";
+
+        PostRequestDto requestDto = FixtureMonkeyUtil.monkey()
+                .giveMeBuilder(PostRequestDto.class)
+                .set("contents", content)
+                .sample();
+        // when
+        post = new Post(requestDto.getContents());
+
+        // then
+        assertEquals(requestDto.getContents(), post.getContents());
+    }
+
+    @DisplayName("게시글 업데이트")
+    @Test
+    void test2() {
+        // given
+        String content = "updateContent";
+
+        PostRequestDto requestDto = FixtureMonkeyUtil.monkey()
+                .giveMeBuilder(PostRequestDto.class)
+                .set("contents", content)
+                .sample();
+        // when
+        post.update(requestDto.getContents());
+
+        // then
+        assertEquals(requestDto.getContents(), post.getContents());
+    }
+}
