132c36b132b91bfe9c5f0d025eb9b6a4a5dfd534
leesewon <leesw1945@gmail.com>
2024-06-24 17:28:22 +0900

    fix: Swagger

--- src/main/java/com/sparta/ottoon/follow/controller/FollowController.java
+++ src/main/java/com/sparta/ottoon/follow/controller/FollowController.java
@@ -4,12 +4,15 @@ import com.sparta.ottoon.auth.service.UserDetailsImpl;
 import com.sparta.ottoon.follow.dto.FollowResponseDto;
 import com.sparta.ottoon.follow.service.FollowService;
 import com.sparta.ottoon.profile.dto.ProfileResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Follow API", description = "Follow API 입니다")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -17,6 +20,7 @@ public class FollowController {
 
     private final FollowService followService;
 
+    @Operation(summary = "createFollow", description = "팔로우 추가 기능입니다.")
     @PostMapping("/follows/users/{userId}")
     public ResponseEntity<FollowResponseDto<Void>> followUser(@PathVariable long userId,
                                                         @AuthenticationPrincipal UserDetailsImpl userDetails){
@@ -26,6 +30,7 @@ public class FollowController {
                 null));
     }
 
+    @Operation(summary = "deleteFollow", description = "팔로우 취소 기능입니다.")
     @PutMapping("/follows/users/{userId}")
     public ResponseEntity<FollowResponseDto<Void>> followCancel(@PathVariable long userId,
                                                           @AuthenticationPrincipal UserDetailsImpl userDetails){
