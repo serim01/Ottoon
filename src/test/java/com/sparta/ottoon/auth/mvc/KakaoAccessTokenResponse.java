de9fe1bfc75754c83a98c3f631369be9238109ef
이종원 <ejong_1@naver.com>
2024-06-24 16:45:11 +0900

    add : auth Dto, Service, Controller test 추가 fix : user, post @setter 삭제 미완성 : KakaoAccessToeknResponse, MockKakaoServerController, MvcTest, SOcialServiceTest

---
+++ src/test/java/com/sparta/ottoon/auth/mvc/KakaoAccessTokenResponse.java
@@ -0,0 +1,14 @@
+package com.sparta.ottoon.auth.mvc;
+//
+//
+//import lombok.Builder;
+//
+//public class KakaoAccessTokenResponse {
+//    private String accessToken;
+//
+//    @Builder
+//    public KakaoAccessTokenResponse(String accessToken) {
+//        this.accessToken=accessToken;
+//    }
+//
+//}
