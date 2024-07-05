c1b53cacee09425cec93149638ce5253585a94e6
serim01 <kkhksl13@gmail.com>
2024-07-05 11:28:40 +0900

    feat: - **팔로워 게시글 목록 조회 기능에 정렬 기준 추가**             - 팔로워 게시글 목록 조회 기능에 작성자명 기준 정렬기능을 추가합니다.             - 응답정보는 기존 게시글 목록 조회기능 응답정보와 동일합니다.             - 페이지네이션                 - 페이지네이션하여 각 페이지 당 게시물 데이터가 5개씩 나오게 합니다. - 작성자명 기준은 writer로 지정하며, 오름차순으로 지정함.

--- src/main/java/com/sparta/ottoon/follow/repository/FollowRepositoryImpl.java
+++ src/main/java/com/sparta/ottoon/follow/repository/FollowRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.sparta.ottoon.follow.repository;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.ottoon.follow.entity.QFollow;
 import com.sparta.ottoon.post.entity.Post;
@@ -8,7 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import static com.sparta.ottoon.post.entity.QPost.post;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,13 +25,34 @@ public class FollowRepositoryImpl implements FollowRepositoryCustom{
         QFollow follow = QFollow.follow;
         QPost post = QPost.post;
 
+        String sort = pageable.getSort().toString();
+        int colonIndex = sort.indexOf(":");
+        String sortType = sort.substring(0, colonIndex).trim();
+
+        // 정렬 조건을 생성
+        List<OrderSpecifier<?>> orderSpecifiers = createOrderSpecifier(sortType);
+
         return queryFactory.selectFrom(post)
                 .innerJoin(follow).on(post.user.id.eq(follow.followUser.id))
                 .where(follow.userId.eq(userId))
-                .orderBy(post.isTop.desc(), post.createdAt.desc())
+                .orderBy(orderSpecifiers.toArray(new OrderSpecifier[0]))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
+    }
+
+    private List<OrderSpecifier<?>> createOrderSpecifier(String sortType) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
+
+        orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, post.isTop));
+
+        if(Objects.equals(sortType, "writer")){
+            orderSpecifiers.add(new OrderSpecifier<>(Order.ASC, post.user.nickname));
+        }else{
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, post.createdAt));
+        }
+
+        return orderSpecifiers;
     }
 }
